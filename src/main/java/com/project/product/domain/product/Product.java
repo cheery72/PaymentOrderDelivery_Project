126e333f0ce2d86d2876e75594a1332a5b12c8ad
김경민 <nutmy52@naver.com>
2022-11-18 23:26:45 +0900

    feature: service test register product
    서비스 물품 등록 테스트

--- src/main/java/com/project/product/domain/product/Product.java
+++ src/main/java/com/project/product/domain/product/Product.java
@@ -2,7 +2,9 @@ package com.project.product.domain.product;
 
 import com.project.product.domain.member.ShoppingBasket;
 import com.project.product.domain.order.Order;
+import com.project.product.dto.ProductRegisterDto;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -41,4 +43,27 @@ public class Product {
     @JoinColumn(name = "shopping_basket_id")
     private ShoppingBasket shoppingBasket;
 
+    @Builder
+    public Product(Long id, String seller, String name, int price, String category, ProductStatus productStatus, List<ProductImage> images, Order order, ShoppingBasket shoppingBasket) {
+        this.id = id;
+        this.seller = seller;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.productStatus = productStatus;
+        this.images = images;
+        this.order = order;
+        this.shoppingBasket = shoppingBasket;
+    }
+
+    public static Product productBuilder(ProductRegisterDto productRegisterDto){
+        return Product.builder()
+                .seller(productRegisterDto.getSeller())
+                .name(productRegisterDto.getName())
+                .price(productRegisterDto.getPrice())
+                .category(productRegisterDto.getCategory())
+                .images(ProductImage.productImageBuilder(productRegisterDto.getImages()))
+                .productStatus(ProductStatus.VERIFICATION)
+                .build();
+    }
 }
