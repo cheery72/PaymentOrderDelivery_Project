3fc908e7794aa7f475dc6675c4d0db10c2c69bd5
김경민 <nutmy52@naver.com>
2023-01-15 08:42:54 +0900

    chore: class file delete

--- src/main/resources/service/BookmarkRepository.java
+++
@@ -1,8 +0,0 @@
-package antigravity.repository;
-
-import antigravity.entity.Bookmark;
-import antigravity.repository.querydsl.BookmarkRepositoryCustom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark,Long>, BookmarkRepositoryCustom {
-}
--- src/main/resources/service/querydsl/BookmarkRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package antigravity.repository.querydsl;
-
-import antigravity.payload.ProductResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface BookmarkRepositoryCustom {
-    Page<ProductResponse> findByss(Boolean liked, Pageable pageable, Long userId);
-}
