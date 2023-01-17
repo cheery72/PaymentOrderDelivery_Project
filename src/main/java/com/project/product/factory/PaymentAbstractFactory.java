5ea3e9f0823088a99bf2137d29e490d085f21acd
김경민 <nutmy52@naver.com>
2023-01-18 02:26:09 +0900

    refactor: enum type check change

--- src/main/java/com/project/product/factory/PaymentAbstractFactory.java
+++ src/main/java/com/project/product/factory/PaymentAbstractFactory.java
@@ -1,5 +1,7 @@
 package com.project.product.factory;
 
+import com.project.product.domain.order.PayType;
+import com.project.product.domain.payment.CardStatus;
 import com.project.product.service.member.MemberService;
 import com.project.product.service.payment.CardService;
 import com.project.product.service.payment.PaymentService;
@@ -15,9 +17,10 @@ public class PaymentAbstractFactory {
     private final MemberService memberService;
 
     public PaymentService getPayType(int point, String payType){
-        if(0 < point && ("ALL".equals(payType) || "POINT".equals(payType))){
+        if(0 < point && (String.valueOf(PayType.ALL).equals(payType)
+                || String.valueOf(PayType.POINT).equals(payType))){
             return memberService;
-        }else if(point == 0 && "CARD".equals(payType)){
+        }else if(point == 0 && String.valueOf(PayType.CARD).equals(payType)){
             return cardService;
         }
 
