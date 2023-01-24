28bfc14951679bc1186d9cb138796c119dba8cbb
김경민 <nutmy52@naver.com>
2023-01-24 21:13:09 +0900

    refactor: create PointService point payment

--- src/main/java/com/project/product/service/point/PointService.java
+++ src/main/java/com/project/product/service/point/PointService.java
@@ -1,6 +1,9 @@
 package com.project.product.service.point;
 
+import com.project.product.domain.member.Member;
 import com.project.product.dto.order.OrderCreateRequest;
+import com.project.product.exception.ClientException;
+import com.project.product.exception.ErrorCode;
 import com.project.product.repository.member.MemberRepository;
 import com.project.product.repository.payment.CardRepository;
 import com.project.product.service.member.MemberService;
@@ -15,10 +18,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class PointService {
 
     private final MemberRepository memberRepository;
-    private final CardRepository cardRepository;
 
     @Transactional
-    public int pointPayment(OrderCreateRequest orderCreateRequest){
-        return 0;
+    public int pointPayment(OrderCreateRequest orderCreateRequest, int discount) {
+
+        Member member = memberRepository.findById(orderCreateRequest.getPurchaser())
+                .orElseThrow(() -> new ClientException(ErrorCode.NOT_FOUND_MEMBER));
+
+        if(memberPointCheck(orderCreateRequest.getUsePoint(), member.getPoint())){
+            return member.memberPointPayment(orderCreateRequest.getTotalPrice(), orderCreateRequest.getUsePoint(), discount);
+        }
+        throw new ClientException(ErrorCode.REJECT_POINT_PAYMENT);
+
     }
+
+    private boolean memberPointCheck(int userPoint, int memberAvailablePoint){
+        return userPoint <= memberAvailablePoint;
+    }
+
 }
